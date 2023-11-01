68582badcd2d3a1a317a1f0dd806a9577311c167
Bowie <pablorognoni.maze@gmail.com>
2023-11-01 13:39:07 -0300

    site_laravel

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="col-md-10 offset-md-1 dashboart-title-container">
+    <h1>Meus Eventos</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboart-events-container">
+    @if(count(events) > 0)
+    @else
+    <p>Voce ainda não tem eventos, <a href="/events/create">criar evento</a></p>
+    @endif
+</div>
+
+@endsection
